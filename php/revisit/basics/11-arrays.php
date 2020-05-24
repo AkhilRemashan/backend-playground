fb462900bdea8262ab19f9307ca883f3e8677925
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-05-24 11:18:55 +0530

    associative

---
+++ php/revisit/basics/11-arrays.php
@@ -0,0 +1,11 @@
+<?php
+
+            $friends = array("Adam", "Bonnie", "Clyde", "Derik");
+
+            $friends[4] = "Earl";
+
+            echo count($friends);
+
+            echo $friends[2];
+
+        ?> 
