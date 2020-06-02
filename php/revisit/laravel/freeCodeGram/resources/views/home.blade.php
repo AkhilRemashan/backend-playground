caa5881ade7b609ef6aaf43146dc02079033dc96
Akhil Remashan <sudonowww@gmail.com>
2020-06-02 10:08:51 -0700

    validation_4000

--- php/revisit/laravel/freeCodeGram/resources/views/home.blade.php
+++ php/revisit/laravel/freeCodeGram/resources/views/home.blade.php
@@ -2,22 +2,37 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=82J-Mgl8DlMAX-ZCE4y&oh=73298cc302c8d28932c0a7faea3fb864&oe=5EFF9D67" alt="" class="img-fluid rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>freeCodeGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>150</strong> posts</div>
+                <div class="pr-4"><strong>23k</strong> followers</div>
+                <div class="pr-4"><strong>212</strong> following</div>
             </div>
+            <div class="pt-2 font-weight-bold">freecodegram.net</div>
+            <div>
+                We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+            </div>
+            <div class=""><a href="#">freecodegram.net</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.107.924.924a/s240x240/101418871_149220720095600_2797518877581952974_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=8xCKgFzItPwAX8ioxYs&oh=0ecea283e7dd5021bf28d34e42f95fdf&oe=5EFF4B09" alt="" class="w-100" />
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/c64.0.548.548a/s150x150/100898919_639147106680041_7770796487488702501_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=hgJVZ7ZOeQEAX-Z-Cyi&oh=3925a95884e89cea8466f1777c4be6d9&oe=5F0044FE" alt="" class="w-100" />
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/c127.0.495.495a/s240x240/100950429_172849147497410_8469189274443807895_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=kXyYvwAsrz8AX-qiZaV&oh=528a85add18e63b8a5dda563612aade7&oe=5EFECE68" alt="" class="w-100" />
+        </div>
+    </div>
+
+
 </div>
 @endsection
--- php/revisit/laravel/freeCodeGram/resources/views/layouts/app.blade.php
+++ php/revisit/laravel/freeCodeGram/resources/views/layouts/app.blade.php
@@ -23,8 +23,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pr-2" style="border-right: 1px solid #333">
+                        <img src="/images/svg/logo.svg" alt="Logo" style="height: 20px;"> 
+                    </div>
+                    <div class="pl-2">freeCodeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
