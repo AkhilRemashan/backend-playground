849595b58d8ddb667ce9e6e3f564c75dae45c854
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2021-05-18 22:42:47 +0530

    rjs-1805

--- JS/React/Rough1/rjs/src/App.js
+++ JS/React/Rough1/rjs/src/App.js
@@ -1,9 +1,13 @@
-
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div>
-      Hey!
+        <h1>My Todos</h1>
+        
+        <Todo text="first" />
+        <Todo text="second" />
+        <Todo text="third" />
     </div>
   );
 }
